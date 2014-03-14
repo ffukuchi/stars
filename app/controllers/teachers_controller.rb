092fd33318ac923f63e1a2b5f48b04dc8e2b9a47
Frank Fukuchi <frank.fukuchi@gmail.com>
2014-03-14 13:35:52 -0400

    got teacher edit & update working

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -16,12 +16,25 @@ class TeachersController < ApplicationController
   end
 
   def edit
+    @teacher = Teacher.find(params[:id])
   end
 
   def update
+    @teacher = Teacher.find(params[:id])
+    if @teacher.update(teacher_params)
+      redirect_to teacher_path(@teacher)
+    else
+      render :new
+    end
   end
 
-  def destory
+  def destroy
   end
+
+private
+
+def teacher_params
+  params.require(:teacher).permit(:last_name, :first_name, :school, :grade, :subject)
+end
 
 end
