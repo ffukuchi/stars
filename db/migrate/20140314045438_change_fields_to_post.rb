9da1cc4198dbceaf35fbb7b3b464543ac1033871
Frank Fukuchi <frank.fukuchi@gmail.com>
2014-08-28 14:59:23 -0400

    migration typo?

--- db/migrate/20140314045438_change_fields_to_post.rb
+++ db/migrate/20140314045438_change_fields_to_post.rb
@@ -1,8 +1,8 @@
 class ChangeFieldsToPost < ActiveRecord::Migration
 
   def change
-    add_column :posts, :like_id, :intger 
-    add_column :posts, :stars, :intger 
+    add_column :posts, :like_id, :integer 
+    add_column :posts, :stars, :integer 
   end
 
 end
